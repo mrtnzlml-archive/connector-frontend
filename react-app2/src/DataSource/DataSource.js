bfa0ba1e0a3b6e05ae4bf525468f69a263cd392b
Martin Zlámal <mrtnzlml@gmail.com>
2016-11-17 19:49:50 +0100

    Add DataSource component

---
+++ react-app2/src/DataSource/DataSource.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import LoginForm from './../LoginForm/LoginForm';
+
+export default class extends React.Component {
+	render() {
+		return (
+			<LoginForm/>
+		);
+	}
+}
