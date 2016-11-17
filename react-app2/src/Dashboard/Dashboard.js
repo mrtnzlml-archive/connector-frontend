aea07e932a2e76c205f4b5312dff97f11c254171
Martin Zlámal <mrtnzlml@gmail.com>
2016-11-17 19:44:03 +0100

    Add date into Dashboard

--- react-app2/src/Dashboard/Dashboard.js
+++ react-app2/src/Dashboard/Dashboard.js
@@ -1,9 +1,36 @@
 import React from 'react';
 
 export default class extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			date: new Date()
+		};
+	}
+
+	componentDidMount() {
+		this.timerID = setInterval(
+			() => this.tick(),
+			1000
+		);
+	}
+
+	componentWillUnmount() {
+		clearInterval(this.timerID);
+	}
+
+	tick() {
+		this.setState({
+			date: new Date()
+		});
+	}
+
 	render() {
 		return (
-			<p>Welcome to the Dashboard!</p>
+			<div>
+				<p>Welcome to the Dashboard!</p>
+				<p>It is {this.state.date.toLocaleTimeString()}.</p>
+			</div>
 		);
 	}
 }
