9f816296673ddf0eb54a78569fc869891477a11c
Martin Zlámal <mrtnzlml@gmail.com>
2016-11-17 16:49:37 +0100

    React builds: run babelRelayPlugin

--- react-app2/config/webpack.config.prod.js
+++ react-app2/config/webpack.config.prod.js
@@ -103,7 +103,9 @@ module.exports = {
         test: /\.(js|jsx)$/,
         include: paths.appSrc,
         loader: 'babel',
-        
+	      query: {
+		      plugins: ['./../scripts/babelRelayPlugin']
+	      }
       },
       // The notation here is somewhat confusing.
       // "postcss" loader applies autoprefixer to our CSS.
