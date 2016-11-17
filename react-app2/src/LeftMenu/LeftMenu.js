698eceeedcfdf0dfd315c4fbdde389867032ef07
Martin Zlámal <mrtnzlml@gmail.com>
2016-11-17 19:50:20 +0100

    Add LeftMenu component

---
+++ react-app2/src/LeftMenu/LeftMenu.js
@@ -0,0 +1,62 @@
+import React from 'react';
+// import Relay from 'react-relay';
+import {Link} from 'react-router'
+
+import './LeftMenu.css';
+
+import {List, ListItem} from 'material-ui/List';
+import Subheader from 'material-ui/Subheader';
+import Divider from 'material-ui/Divider';
+import Checkbox from 'material-ui/Checkbox';
+
+class LeftMenu extends React.Component {
+	render() {
+
+		//console.log(this.props.dataSource);
+
+		return (
+			<div className="LeftMenu">
+				<List>
+					<Subheader>Adeira<strong>:connector</strong></Subheader>
+					<Link to="/">
+						<ListItem
+							primaryText="Dashboard"
+						/>
+					</Link>
+					<Link to="/data-sources">
+						<ListItem
+							primaryText="Data Sources"
+							secondaryText="List all available data sources"
+						/>
+					</Link>
+				</List>
+				<Divider/>
+				<List>
+					<Subheader>Hangout Notifications</Subheader>
+					<ListItem
+						leftCheckbox={<Checkbox />}
+						primaryText="Notifications"
+						secondaryText="Allow notifications"
+					/>
+				</List>
+			</div>
+		);
+	}
+}
+
+LeftMenu.propTypes = {
+	// dataSource: React.PropTypes.object.isRequired
+};
+
+export default LeftMenu;
+// export default Relay.createContainer(LeftMenu, {
+// 	fragments: {
+// 		// Define a fragment with a name matching the `dataSource` prop expected above
+// 		dataSource: () => Relay.QL`
+//       fragment on DataSource {
+//         id,
+//         name
+//       }
+//     `
+// 	}
+// });
