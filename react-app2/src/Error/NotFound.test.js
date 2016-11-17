30b43dc548fb5e7b330a59d49a2a680c67315d25
Martin Zlámal <mrtnzlml@gmail.com>
2016-11-17 19:32:02 +0100

    Add NotFound component test

--- react-app2/src/Error/NotFound.test.js
+++ react-app2/src/Error/NotFound.test.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import LoginForm from './LoginForm';
+import NotFound from './../Error/NotFound';
 
 it('renders without crashing', () => {
 	const div = document.createElement('div');
 	ReactDOM.render(
-		<LoginForm/>,
+		<NotFound/>,
 		div
 	);
 });
