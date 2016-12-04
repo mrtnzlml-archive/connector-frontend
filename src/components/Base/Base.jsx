9a7d89607b2f0d602acde401d9ce95756daf586e
Martin Zlámal <mrtnzlml@gmail.com>
2016-12-04 15:16:56 +0100

    Remove LoginForm from Base component

--- src/components/Base/Base.jsx
+++ src/components/Base/Base.jsx
@@ -1,21 +1,9 @@
 import React from 'react';
 import LeftMenu from '../LeftMenu/LeftMenu';
 import './Base.css';
-import LoginForm from '../LoginForm/LoginForm';
 
 class Base extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			loggedIn: true, //FIXME
-		};
-	}
-
 	render() {
-		if (!this.state.loggedIn) {
-			return <LoginForm/>;
-		}
-
 		return (
 			<div id="App">
 				<div id="sidebar">
