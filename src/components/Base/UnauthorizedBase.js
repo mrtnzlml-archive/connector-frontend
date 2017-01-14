4af940b87f268ef84b0c8f9fc3123d2b64313c31
Martin Zlámal <mrtnzlml@gmail.com>
2017-01-14 09:49:00 +0100

    Style login page

--- src/components/Base/UnauthorizedBase.js
+++ src/components/Base/UnauthorizedBase.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './AuthorizedBase.css';
+import './UnauthorizedBase.css';
 import LoginForm from '../../scenes/Sign/Login/Form';
 import PaperToastContainer from '../PaperToast/PaperToastContainer';
 
@@ -7,10 +7,9 @@ class UnauthorizedBase extends React.Component {
 	render() {
 		return (
 			<div id="App">
-				<div id="body">
+					<h2>Please log in</h2>
 					<LoginForm/>
-					<PaperToastContainer/>
-				</div>
+				<PaperToastContainer/>
 			</div>
 		);
 	}
