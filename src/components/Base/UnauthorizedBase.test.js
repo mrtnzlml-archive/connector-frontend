d1dc278a20b650a757d4b7a2104b3e52408aa6a7
Martin Zlámal <mrtnzlml@gmail.com>
2017-01-14 10:12:21 +0100

    UnauthorizedBase test: remove MuiThemeProvider

--- src/components/Base/UnauthorizedBase.test.js
+++ src/components/Base/UnauthorizedBase.test.js
@@ -2,20 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import UnauthorizedBase from './UnauthorizedBase';
 
-import ApolloClient from 'apollo-client'; //FIXME: only because of Material-UI
+import ApolloClient from 'apollo-client'; //FIXME: only because of Apollo
 import {ApolloProvider} from 'react-apollo';
 
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'; //FIXME: only because of Material-UI
 import injectTapEventPlugin from 'react-tap-event-plugin';
 injectTapEventPlugin(); // needed for onTouchTap (http://stackoverflow.com/a/34015469/988941)
 
 it('renders without crashing', () => {
 	ReactDOM.render(
-		<MuiThemeProvider>
-			<ApolloProvider client={new ApolloClient}>
-				<UnauthorizedBase/>
-			</ApolloProvider>
-		</MuiThemeProvider>,
+		<ApolloProvider client={new ApolloClient}>
+			<UnauthorizedBase/>
+		</ApolloProvider>,
 		document.createElement('div')
 	);
 });
