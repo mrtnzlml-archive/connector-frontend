6713589aed45dc140cf17295b41d79713c0b384c
Martin Zlámal <mrtnzlml@gmail.com>
2017-02-16 09:36:56 +0100

    MuiTheme: change base colors

--- src/services/MaterialUI/MuiTheme.js
+++ src/services/MaterialUI/MuiTheme.js
@@ -1,11 +1,11 @@
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
-import {green100, green500, green700} from 'material-ui/styles/colors';
+import {lightBlue100, lightBlue500, lightBlue700} from 'material-ui/styles/colors';
 
 export default getMuiTheme({
 	palette: {
-		primary1Color: green500,
-		primary2Color: green700,
-		primary3Color: green100,
+		primary1Color: lightBlue500,
+		primary2Color: lightBlue700,
+		primary3Color: lightBlue100,
 	},
 }, {
 	avatar: {
