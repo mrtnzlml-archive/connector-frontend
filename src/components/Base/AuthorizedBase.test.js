51b7757d8bb1b956a0057f65cb1de5fe1048fba5
Martin Zlámal <mrtnzlml@gmail.com>
2017-02-16 09:01:11 +0100

    AuthorizedBase: use redux storage in tests

--- src/components/Base/AuthorizedBase.test.js
+++ src/components/Base/AuthorizedBase.test.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AuthorizedBase from './AuthorizedBase';
+import {Provider} from 'react-redux'
+import ReduxStore from '../../services/ReduxStore';
 
 import injectTapEventPlugin from 'react-tap-event-plugin';
 injectTapEventPlugin(); // needed for onTouchTap (https://github.com/callemall/material-ui#react-tap-event-plugin)
 
 it('renders without crashing', () => {
 	ReactDOM.render(
-		<AuthorizedBase/>,
+		<Provider store={ReduxStore}>
+			<AuthorizedBase/>
+		</Provider>,
 		document.createElement('div')
 	);
 });
