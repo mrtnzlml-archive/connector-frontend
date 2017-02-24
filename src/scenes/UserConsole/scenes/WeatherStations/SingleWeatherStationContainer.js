5939447cd5c6ef8fb24996c5a03a3a8dc6d9aa53
Martin Zlámal <mrtnzlml@gmail.com>
2017-02-24 17:36:55 +0100

    Style single weather station page

--- src/scenes/UserConsole/scenes/WeatherStations/SingleWeatherStationContainer.js
+++ src/scenes/UserConsole/scenes/WeatherStations/SingleWeatherStationContainer.js
@@ -5,23 +5,30 @@ import {browserHistory} from 'react-router';
 import Graph from './SingleWeatherStationGraph';
 import Documentation from './SingleWeatherStationDoc';
 import RaisedButton from 'material-ui/RaisedButton';
+import SelectField from 'material-ui/SelectField';
+import MenuItem from 'material-ui/MenuItem';
+import Formsy from 'formsy-react';
+import {FormsyDate, FormsyText} from 'formsy-material-ui/lib';
 
 const SingleWeatherStationContainer = class extends React.Component {
 
 	constructor(props) {
 		super(props);
 
+		let allowedInterpolations = ['natural', 'step', 'monotone', 'linear'];
+		let interpolation = localStorage.getItem('interpolation');
 		this.state = {
-			interpolation: 'natural',
+			interpolation: allowedInterpolations.includes(interpolation) ? interpolation : 'natural',
 		};
 
 		// This binding is necessary to make `this` work in the callback
-		this.curve = this.curve.bind(this);
+		this.handleCurve = this.handleCurve.bind(this);
 	}
 
-	curve(interpolation) {
+	handleCurve(event, index, value) {
+		localStorage.setItem('interpolation', value);
 		this.setState(prevState => ({
-			interpolation: interpolation
+			interpolation: value
 		}));
 	};
 
@@ -36,34 +43,74 @@ const SingleWeatherStationContainer = class extends React.Component {
 			browserHistory.push('/');
 		}
 
+		let graphStyle = {marginTop: '2rem', marginBottom: '2rem'};
 		return <div>
 			<h2>Weather station {station.name}</h2>
 
 			{/* TODO: current temperature (last record preview) */}
 
-			<RaisedButton label="Natural interpolation" onClick={() => this.curve('natural')}/>
-			<RaisedButton label="Step interpolation" onClick={() => this.curve('step')}/>
+			<SelectField name="interpolation" floatingLabelText="Graph interpolation" value={this.state.interpolation} onChange={this.handleCurve} style={{
+				float: 'left',
+				marginRight: '1rem',
+			}}>
+				<MenuItem value="natural" primaryText="Natural"/>
+				<MenuItem value="step" primaryText="Step"/>
+				<MenuItem value="monotone" primaryText="Monotone"/>
+				<MenuItem value="linear" primaryText="Linear"/>
+			</SelectField>
+			<Formsy.Form>
+				<FormsyDate
+					name="untilDate"
+					floatingLabelText="Show records until date"
+					container="inline"
+					mode="landscape"
+					maxDate={new Date()}
+					defaultDate={new Date()}
+					style={{
+						float: 'left',
+						marginRight: '1rem',
+					}}
+				/>
+				<FormsyText name="gap" floatingLabelText="Gap between records" type="number" defaultValue={1}/> {/* FIXME: validace rozsahů na straně serveru */}
+			</Formsy.Form>
 
-			<h3>Temperature history</h3>
-			<Graph data={station.allRecords} dataKeys={['indoorTemperature', 'outdoorTemperature']} interpolation={this.state.interpolation}/>
+			<div style={graphStyle}>
+				<h3>Temperature history</h3>
+				<Graph data={station.allRecords} dataKeys={['indoorTemperature', 'outdoorTemperature']} interpolation={this.state.interpolation}/>
+			</div>
 
-			<h3>Pressure history</h3>
-			<Graph data={station.allRecords} dataKeys={['absolutePressure', 'relativePressure']} interpolation={this.state.interpolation}/>
+			<div style={graphStyle}>
+				<h3>Pressure history</h3>
+				<Graph data={station.allRecords} dataKeys={['absolutePressure', 'relativePressure']} interpolation={this.state.interpolation}/>
+			</div>
 
-			<h3>Humidity history</h3>
-			<Graph data={station.allRecords} dataKeys={['indoorHumidity', 'outdoorHumidity']} interpolation={this.state.interpolation}/>
+			<div style={graphStyle}>
+				<h3>Humidity history</h3>
+				<Graph data={station.allRecords} dataKeys={['indoorHumidity', 'outdoorHumidity']} interpolation={this.state.interpolation}/>
+			</div>
 
-			<h3>Wind speed and gust history</h3>
-			<Graph data={station.allRecords} dataKeys={['windSpeed', 'windGust']} interpolation={this.state.interpolation}/>
+			<div style={graphStyle}>
+				<h3>Wind speed and gust history</h3>
+				<Graph data={station.allRecords} dataKeys={['windSpeed', 'windGust']} interpolation={this.state.interpolation}/>
+			</div>
 
 			<Documentation wsId={station.id}/>
+
+			<div style={{borderTop: '3px solid indianred', marginTop: '5rem', paddingTop: '3rem'}}>
+				<h2 style={{color: 'indianred'}}>Danger zone</h2>
+				<p>
+					Rename weather station
+				</p>
+				<div>
+					<RaisedButton label="Delete weather station" backgroundColor="indianred" labelColor="white"/>
+				</div>
+			</div>
 
 		</div>
 	};
 
 };
 
-//TODO: oddělit dotaz na více menších dotazů?
 export default graphql(gql`
   query ($wsId: ID!, $first: Int!) {
     station: weatherStation(id: $wsId) {
--- src/scenes/UserConsole/scenes/WeatherStations/SingleWeatherStationGraph.js
+++ src/scenes/UserConsole/scenes/WeatherStations/SingleWeatherStationGraph.js
@@ -1,22 +1,53 @@
 import React from 'react';
 import {LineChart, Line, XAxis, YAxis, ResponsiveContainer, Tooltip} from 'recharts';
 
+let tickFormatterX = (timestamp) => {
+	//TODO: nejen datum, ale i čas!
+	let date = new Date(Date.parse(timestamp));
+	return date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds();
+};
+
+let renderTooltip = (props) => {
+	let {payload} = props;
+	if (!payload.length) {
+		return;
+	}
+	return <div>
+		{payload[0].value}
+		<br/>
+		{payload[1].value}
+	</div>;
+};
+
 export default (props) => {
 
 	return <ResponsiveContainer width="100%" height={100}>
 		<LineChart data={props.data} syncId="singleWS">
-			<XAxis dataKey="creationDate"/>
-			<YAxis type="number" domain={['auto', 'auto']}/>
-			{props.dataKeys.map((dataKey) => {
+
+			<XAxis
+				dataKey="creationDate"
+				tickFormatter={tickFormatterX}
+				minTickGap={10}
+			/>
+			<YAxis
+				hide={true}
+				domain={['auto', 'auto']}
+			/>
+
+			{props.dataKeys.map((dataKey, index) => {
+				let colors = ['0288d1', '0fb8ad'];
 				return <Line
 					key={dataKey}
 					type={props.interpolation}
 					dataKey={dataKey}
-					stroke="#8884d8"
+					stroke={`#${colors[index]}`}
 					isAnimationActive={false}
-					dot={{r: 2}}/>
+					dot={{r: 2}}
+				/>
 			})}
-			<Tooltip isAnimationActive={false}/>
+
+			<Tooltip content={renderTooltip} isAnimationActive={false}/>
+
 		</LineChart>
 	</ResponsiveContainer>;
 
