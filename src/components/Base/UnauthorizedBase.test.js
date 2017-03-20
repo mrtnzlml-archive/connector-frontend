6e3f1480abd52ec60153e23e699c7f8764f58ea5
Martin Zlámal <mrtnzlml@gmail.com>
2017-03-20 15:37:49 +0100

    Improve base tests

--- src/components/Base/UnauthorizedBase.test.js
+++ src/components/Base/UnauthorizedBase.test.js
@@ -1,18 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import UnauthorizedBase from './UnauthorizedBase';
-
-import ApolloClient from 'apollo-client'; //FIXME: only because of Apollo
-import {ApolloProvider} from 'react-apollo';
-
-import injectTapEventPlugin from 'react-tap-event-plugin';
-injectTapEventPlugin(); // needed for onTouchTap (http://stackoverflow.com/a/34015469/988941)
+import {ApplicationContext} from 'services/TestUtils';
 
 it('renders without crashing', () => {
 	ReactDOM.render(
-		<ApolloProvider client={new ApolloClient}>
+		<ApplicationContext>
 			<UnauthorizedBase/>
-		</ApolloProvider>,
+		</ApplicationContext>,
 		document.createElement('div')
 	);
 });
